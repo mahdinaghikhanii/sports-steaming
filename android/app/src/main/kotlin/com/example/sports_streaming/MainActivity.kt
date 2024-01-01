edf26bd4b83e61f1c12ec39822cbe82a0c208609
Mahdinkh7 <mahdinkh7@gmail.com>
2024-01-01 23:44:57 +0330

    f

---
+++ android/app/src/main/kotlin/com/example/sports_streaming/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sports_streaming
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/root.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'home/home.dart';
+
+const int homeIndex = 0;
+const int cartIndex = 1;
+const int profileIndex = 2;
+
+class RootScrean extends StatefulWidget {
+  const RootScrean({super.key});
+
+  @override
+  State<RootScrean> createState() => _RootScreanState();
+}
+
+class _RootScreanState extends State<RootScrean> {
+  int selectedScreenIndex = homeIndex;
+  final List<int> _history = [];
+
+  final GlobalKey<NavigatorState> _homeKey = GlobalKey();
+  final GlobalKey<NavigatorState> _cartKey = GlobalKey();
+  final GlobalKey<NavigatorState> _profileKey = GlobalKey();
+
+  late final map = {
+    homeIndex: _homeKey,
+    cartIndex: _cartKey,
+    profileIndex: _profileKey,
+  };
+
+  Future<bool> _onWillPop() async {
+    final NavigatorState currentSelectedTabNavigatorState =
+        map[selectedScreenIndex]!.currentState!;
+    if (currentSelectedTabNavigatorState.canPop()) {
+      currentSelectedTabNavigatorState.pop();
+      return false;
+    } else if (_history.isNotEmpty) {
+      setState(() {
+        selectedScreenIndex = _history.last;
+        _history.removeLast();
+      });
+      return false;
+    }
+
+    return true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+          body: IndexedStack(
+            index: selectedScreenIndex,
+            children: [
+              _navigator(_homeKey, homeIndex, const HomePages()),
+              _navigator(_cartKey, cartIndex, const HomePages()),
+              _navigator(_profileKey, profileIndex, const HomePages()),
+              _navigator(_profileKey, profileIndex, const HomePages())
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.home), label: "Home"),
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.cart), label: "Live"),
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.person), label: "Library"),
+              BottomNavigationBarItem(
+                  icon: Icon(CupertinoIcons.person), label: "Search"),
+            ],
+            currentIndex: selectedScreenIndex,
+            onTap: (selectIndex) {
+              setState(() {
+                selectedScreenIndex = selectIndex;
+              });
+            },
+          )),
+    );
+  }
+
+  Widget _navigator(GlobalKey key, int index, Widget child) {
+    return key.currentState == null && selectedScreenIndex != index
+        ? Container()
+        : Navigator(
+            key: key,
+            onGenerateRoute: (settings) => MaterialPageRoute(
+                builder: (context) => Offstage(
+                    offstage: selectedScreenIndex != index, child: child)));
+  }
+}
